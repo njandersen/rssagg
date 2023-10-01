d5e0294afaf60e9345cd7df17a8f94f471755dae
njandersen <njandersen22@gmail.com>
2023-09-30 22:49:52 -0400

    get server up and running

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	
+
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT not found in environment variables")
+	}
+
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Handler: router,
+		Addr: ":" + port,
+	}
+
+	log.Printf("Server is running on port %v", port)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("PORT is", port)
+}
